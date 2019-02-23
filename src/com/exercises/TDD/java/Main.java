119d33fbe9c2134595a76cee7a59057c32740a71
Aida Sabaghmoghadam <aida.sabbagh@gmail.com>
2019-02-23 17:14:56 -0500

    Edit the if statement to check for existence of 5 at left.

--- src/com/exercises/TDD/java/Main.java
+++ src/com/exercises/TDD/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
         for ( int givenNumber = 1; givenNumber<=100; givenNumber++){
             if (multipleOfFifteen(givenNumber))
                 System.out.println(gameAnswerFizzBuzz(givenNumber));
-            else if (multipleOfFive(givenNumber))
+            else if (multipleOfFive(givenNumber) || consistOfNumberFiveAtLeft(givenNumber))
                 System.out.println(gameAnswerBuzz(givenNumber));
             else if (multipleOfThree(givenNumber) || consistOfNumberThreeAtRight(givenNumber) || consistOfNumberThreeAtLeft(givenNumber))
                 System.out.println(gameAnswerFizz(givenNumber));
