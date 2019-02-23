d91e9fa3e2f83976df2c30b9e3eca3c167d9c19c
Aida Sabaghmoghadam <aida.sabbagh@gmail.com>
2019-02-23 18:40:00 -0500

    Remove the getNumber method from GameAnswer and constructor

--- src/com/exercises/TDD/java/GameAnswer.java
+++ src/com/exercises/TDD/java/GameAnswer.java
@@ -1,9 +1,5 @@
 public abstract class GameAnswer {
     private int number;
 
-    public GameAnswer(int number) {
-        this.number = number;
-    }
-
     public abstract AnswerStatus getAnswerStatus();
 }
