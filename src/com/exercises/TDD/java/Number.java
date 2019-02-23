e5cc62e7b1764fd1ac4a502ddfcc58e72f23518f
Aida Sabaghmoghadam <aida.sabbagh@gmail.com>
2019-02-23 18:19:25 -0500

    Delete two variables in Number.class

--- src/com/exercises/TDD/java/Number.java
+++ src/com/exercises/TDD/java/Number.java
@@ -1,12 +1,8 @@
 public class Number extends GameAnswer {
 
-    private  GameAnswer gameAnswer;
-    private int number;
-
     public Number(int number) {
         super(number);
     }
-
     @Override
     public AnswerStatus getAnswerStatus() {
         return null;
