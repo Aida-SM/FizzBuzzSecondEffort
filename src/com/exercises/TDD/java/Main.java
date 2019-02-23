0a2e801f395c4b3b4d1419bbb5901087f96b477d
Aida Sabaghmoghadam <aida.sabbagh@gmail.com>
2019-02-23 17:26:11 -0500

    Edit the if statement to print FizzBuzz if number is multiple of 3 and consist of 5 at left

--- src/com/exercises/TDD/java/Main.java
+++ src/com/exercises/TDD/java/Main.java
@@ -7,6 +7,8 @@ public class Main {
         for ( int givenNumber = 1; givenNumber<=100; givenNumber++){
             if (multipleOfFifteen(givenNumber))
                 System.out.println(gameAnswerFizzBuzz(givenNumber));
+            else if (multipleOfThree(givenNumber)&& consistOfNumberFiveAtLeft(givenNumber))
+                System.out.println(gameAnswerFizzBuzz(givenNumber));
             else if (multipleOfFive(givenNumber) || consistOfNumberFiveAtLeft(givenNumber))
                 System.out.println(gameAnswerBuzz(givenNumber));
             else if (multipleOfThree(givenNumber) || consistOfNumberThreeAtRight(givenNumber) || consistOfNumberThreeAtLeft(givenNumber))
